c02c5be7bd5fcadf5f629f81b0dcb6f5e2313e50
Tomoki <84486284+tomoki-rks@users.noreply.github.com>
2021-06-04 11:41:23 +0900

    initial commit

---
+++ src/main/java/com/example/gitbranchpractice/GitBranchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitbranchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBranchPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBranchPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitbranchpractice/GitBranchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitbranchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBranchPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
