7bf4103c75821f53e1a49d57d49201bf6e27f212
DaikiTakayama <84486185+DaikiTakayama@users.noreply.github.com>
2021-06-04 12:11:06 +0900

    ログアウト機能を実装しました。 (#2)
    * frist commit
    
    * Conflict resolution

--- src/main/java/com/example/gitbranchpractice/Car.java
+++ src/main/java/com/example/gitbranchpractice/Car.java
@@ -1,7 +1,12 @@
 package com.example.gitbranchpractice;
 
 public class Car {
+
+    private int speed = 10;
+    private int weight =1000;
+
     private int speed() {
-        return 50;
+        return 500;
     }
+
 }
